d97c2177c56508afc1850e6953962d85e2f542ad
lotte <lottehouwen@hotmail.com>
2018-02-28 13:37:16 +0100

    2e kans - 1

--- src/owe6/eindopdrachtlottehouwen/Virus.java
+++ src/owe6/eindopdrachtlottehouwen/Virus.java
@@ -1,44 +1,53 @@
-
 package owe6.eindopdrachtlottehouwen;
 
+import java.util.Comparator;
+
 /**
- * Class waarin virusobjecten worden gemaakt. getters en setters komen hier tot stand.
- * Nog geen informatie opgehaald uit bestand met deze class en getters. Wordt nog aan gewerkt.
- * @author lotte
- * @version 1.00
- * @since 07-02-2018 (7 feb. 2018)
+ * Class waarin virusobjecten worden gemaakt. getters en setters komen hier tot
+ * stand en door middel van de comperator worden virusid's gesorteerd.
+ *
+ * @author Lotte Houwen
+ * @version 2.0
+ * @since 28-02-2018 (28 feb. 2018)
  */
-public class Virus { 
+public class Virus {// implements Comparable {
+
+    private int virus_id;
+    private String virusnaam;
+    private String virusclasse;
+    private int host_id;
+    private String hostnaam;
+
+    public static int sorteerMethode;
 
     /**
+     * Constructor.
      *
      * @param virus_id
      * @param virusnaam
      * @param virusclasse
-     * @param hostid
+     * @param host_id
      * @param hostnaam
      */
-    public Virus(int virus_id, String virusnaam, String virusclasse, int hostid, String hostnaam) {
+    public Virus(int virus_id, String virusnaam, String virusclasse, int host_id, String hostnaam) {
         this.virus_id = virus_id;
         this.virusnaam = virusnaam;
         this.virusclasse = virusclasse;
-        this.hostid = hostid;
+        this.host_id = host_id;
+        this.hostnaam = hostnaam;
     }
-    private int virus_id;
-    private String virusnaam;
-    private String virusclasse;
-    private int hostid;
-    private String hostnaam;
 
     /**
+     * Virus_id getter.
      *
-     * @return
+     * @return virus_id
      */
     public int getVirus_id() {
         return virus_id;
     }
 
     /**
+     * Virus_id setter.
      *
      * @param virus_id
      */
@@ -47,14 +56,16 @@ public class Virus {
     }
 
     /**
+     * Virusnaam getter.
      *
-     * @return
+     * @return virusnaam
      */
     public String getVirusnaam() {
         return virusnaam;
     }
 
     /**
+     * Virusnaam setter.
      *
      * @param virusnaam
      */
@@ -63,14 +74,16 @@ public class Virus {
     }
 
     /**
+     * Virusclasse getter.
      *
-     * @return
+     * @return virusclasse
      */
     public String getVirusclasse() {
         return virusclasse;
     }
 
     /**
+     * Virusclasse setter.
      *
      * @param virusclasse
      */
@@ -79,30 +92,34 @@ public class Virus {
     }
 
     /**
+     * Host_id getter.
      *
-     * @return
+     * @return host_id
      */
-    public int getHostid() {
-        return hostid;
+    public int getHost_id() {
+        return host_id;
     }
 
     /**
+     * HostSet setter.
      *
-     * @param hostid
+     * @param host_id
      */
-    public void setHostid(int hostid) {
-        this.hostid = hostid;
+    public void setHost_id(int host_id) {
+        this.host_id = host_id;
     }
 
-    /**
+    /** 
+     * hostnaam getter.
      *
-     * @return
+     * @return hostnaam
      */
     public String getHostnaam() {
         return hostnaam;
     }
-
-    /**
+    
+     /**
+     * hostnaam setter.
      *
      * @param hostnaam
      */
@@ -110,4 +127,43 @@ public class Virus {
         this.hostnaam = hostnaam;
     }
 
+    /** 
+     * Pakt 2 virusobjecten, kijkt vervolgens naar de virusid's en sorteerd de 
+     * getallen van klein naar groot zodat de lijsten gesorteerd terugkomen.
+     */
+    
+    public static Comparator<Virus> VirusIDComparator = new Comparator<Virus>() {    //Huub
+
+        @Override
+        public int compare(Virus vir1, Virus vir2) {
+
+            Integer virID1 = vir1.getVirus_id();
+            Integer virID2 = vir2.getVirus_id();
+
+            return virID1.compareTo(virID2);
+
+        }
+
+    };
+
+    /*
+    @Override
+    public int compareTo(Object o) {
+         if (o instanceof Virus) {
+            Virus that = (Virus) o;
+            switch (sorteerMethode) {
+                case 0:
+                    return this.virus_id - that.virus_id;
+                case 1:
+                    return this.virusclasse.compareTo(that.virusclasse);
+                case 2:
+                    return this.hostSet.size() - that.hostSet.size();
+                default:
+                    return 0;
+            }
+        }
+        return 0;
+    }
+     
+     */
 }
